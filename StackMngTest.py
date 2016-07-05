855116cdecc22ce61dc204d7b6be9627b8fd9d1b
rek <rek01@126.com>
2016-07-05 08:32:26 +0800

    some tests

---
+++ StackMngTest.py
@@ -0,0 +1,17 @@
+
+
+import calc
+
+lst = [ 3, 4, 5, 90 ]
+stkMng = calc.StackMng(lst)
+stkMng.push('aaa')
+print stkMng.pop()
+print stkMng.pop()
+print stkMng.pop()
+# print stkMng.pop()
+# print stkMng.pop()
+# print stkMng.pop()
+# print stkMng.pop()
+# print stkMng.pop()
+
+
